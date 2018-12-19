1d93d81f3c0557474995aac4d707ed1cf6c7c847
Lane le Prevost-Smith <lanelps@gmail.com>
2018-12-19 15:16:40 +1300

    added jquery

---
+++ main.js
@@ -0,0 +1,54 @@
+$( "#clear" ).click(function() {
+    $( "#display" ).text( 0 );
+  });
+
+  $( "#decimal" ).click(function() {
+    $( "#display" ).text( "." );
+  });
+  $( "#equals" ).click(function() {
+    $( "#display" ).text( "=" );
+  });
+
+  $( "#zero" ).click(function() {
+    $( "#display" ).text( "0" );
+  });
+  $( "#one" ).click(function() {
+    $( "#display" ).text( "1" );
+  });
+  $( "#two" ).click(function() {
+    $( "#display" ).text( "2" );
+  });
+  $( "#three" ).click(function() {
+    $( "#display" ).text( "3" );
+  });
+  $( "#four" ).click(function() {
+    $( "#display" ).text( "4" );
+  });
+  $( "#five" ).click(function() {
+    $( "#display" ).text( "5" );
+  });
+  $( "#six" ).click(function() {
+    $( "#display" ).text( "6" );
+  });
+  $( "#seven" ).click(function() {
+    $( "#display" ).text( "7" );
+  });
+  $( "#eight" ).click(function() {
+    $( "#display" ).text( "8" );
+  });
+  $( "#nine" ).click(function() {
+    $( "#display" ).text( "9" );
+  });
+
+  $( "#add" ).click(function() {
+    $( "#display" ).text( "+" );
+  });
+  $( "#subtract" ).click(function() {
+    $( "#display" ).text( "-" );
+  });
+  $( "#multiply" ).click(function() {
+    $( "#display" ).text( "*" );
+  });
+  $( "#divide" ).click(function() {
+    $( "#display" ).text( "/" );
+  });
